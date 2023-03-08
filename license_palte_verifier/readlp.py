a225f39d7b08b8b4b5d25cf6a76f664782f72fb1
alon satchi <alonsatchi@gmail.com>
2023-03-08 11:38:24 +0200

    reorder

--- license_palte_verifier/readlp.py
+++ license_palte_verifier/readlp.py
@@ -4,13 +4,11 @@ import re
 import glob
 
 def isValidLicensePlate(img):
-    pytesseract.tesseract_cmd = r'C:/Users/u9092788/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
-    img = cv2.imread(img)
+    pytesseract.tesseract_cmd = r'C:/Users/u9092788/AppData/Local/Programs/Tesseract-OCR/tesseract.exe' #FIXME: change to tesseract path on the linux machine
     cropped = img[0:80, 12:150]
     denoised = cv2.fastNlMeansDenoising( cropped, None, 60, 5, 23)  
     denoised = cv2.cvtColor(denoised, cv2.COLOR_BGR2GRAY)
     denoised = cv2.inRange(denoised, 0, 50)
-    
     cv2.imwrite("cleaned.png", denoised)
     cv2.imshow("image", img)
     cv2.imshow("denoised", denoised)
@@ -25,11 +23,11 @@ def isValidLicensePlate(img):
     else:
         return False
     
-assert isValidLicensePlate("C:/Users/u9092788/Downloads/someimages/75000.png") == True
-assert isValidLicensePlate("C:/Users/u9092788/Desktop/75312.png") == False
 
-for img in glob.glob('C:/Users/u9092788/Downloads/someimages/*.png'):
-    if(isValidLicensePlate(img)):
-        print("valid license plate")
-    else:
-        print("invalid license plate")
+
+# for path in glob.glob('C:/Users/u9092788/Downloads/someimages/*.png'):
+#     img = cv2.imread(path)
+#     if(isValidLicensePlate(img)):
+#         print("valid license plate")
+#     else:
+#         print("invalid license plate")
---
+++ test.py
@@ -0,0 +1,4 @@
+from license_palte_verifier.readlp import *
+#change to valid pathes
+assert isValidLicensePlate("C:/Users/u9092788/Downloads/someimages/75000.png") == True
+assert isValidLicensePlate("C:/Users/u9092788/Desktop/75312.png") == False
